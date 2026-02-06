261ef936999babfba4dd79ca031e7003695e8a6d
JAGDEEP SINGH BRAR <65875944+deepbrar87@users.noreply.github.com>
2026-02-06 15:34:46 +0530

    Add files via upload

---
+++ HR_25_alphabetrangoli.py
@@ -0,0 +1,24 @@
+''' 
+ Python Programs in Hacker Rank 
+ BY: Jagdeep Singh Brar 
+ Program Description: alphabet rangoli
+'''
+#
+
+
+def print_rangoli(size):
+    # your code goes here
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    data = [alpha[i] for i in range(size)]
+    items = list(range(size))
+    items = items[:-1]+items[::-1]
+    for i in items:
+        temp = data[-(i+1):]
+        row = temp[::-1]+temp[1:]
+        print("-".join(row).center(n*4-3, "-"))
+
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangol
